65c29c4b0f6a39e38cb780b9ff3d35f8356d0ddb
= <jacex17@gmail.com>
2019-08-30 14:23:34 -0400

    Add friendlist functionality

--- routes/friends.js
+++ routes/friends.js
@@ -2,12 +2,6 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
 
-<<<<<<< HEAD
-// SEARCH USERS
-router.get('/searchUser', (req, res, next) => {
-    // console.log("The user ID is " + req.user_.id)
-    res.render('friendviews/searchUserForm');
-=======
 //SEARCH USERS
 router.get('/searchUser', (req, res, next) =>{
     console.log(req.query.userToSearch);
@@ -25,27 +19,11 @@ router.get('/searchUser', (req, res, next) =>{
     })
     .catch((err) => console.log("An error just happened ", err))
 }
->>>>>>> 823b29d6e73cce45dd6af1398a3e9ee875e5ef84
 })
 
 //SHOW FRIENDLIST
 
 router.get('/friendlist', (req, res, next) => {
-<<<<<<< HEAD
-    res.render('friendviews/friendList');
-    console.log("the user ID is " + req.user._id)
-    User.findById(req.user._id)
-        .then(theUser => {
-            User.getFriends(theUser, function (err, friendship) {
-                // console.log(friendship);
-                res.render('friendviews/friendList', { theUser, friendship });
-            })
-
-        })
-        .catch((err) => console.log("An err just happened", err))
-    // User.getFriends(req.user._id);
-
-=======
     console.log("the user ID is " + req.user._id)
     User.findById(req.user._id)
     .then(theUser => {
@@ -58,7 +36,6 @@ router.get('/friendlist', (req, res, next) => {
     .catch((err)=> console.log("An err just happened", err))
        // User.getFriends(req.user._id);
    
->>>>>>> 823b29d6e73cce45dd6af1398a3e9ee875e5ef84
 })
 
 // router.post('/AddNewFriend', (req, res, next) => {
@@ -72,19 +49,9 @@ router.post('/AddNewFriend', (req, res, next) => {
     User.findOne({ username: req.body.name })
         .then(user => {
             console.log(user)
-<<<<<<< HEAD
-            if (!user) {
-                req.flash('error', 'There is no username with that name');
-                res.redirect('/searchUser');
-            } else {
-                User.requestFriend(req.user._id, user._id);
-                res.render('friendviews/friendList');
-            }
-=======
             User.requestFriend(req.user._id, user._id);
             res.render('friendviews/friendList');
             // }
->>>>>>> 823b29d6e73cce45dd6af1398a3e9ee875e5ef84
         })
         .catch((err) => console.log("An error just happened ", err))
 })
