ec3c46f510e4250125f788c3bd4f2ef3b6550963
Fabricio Policarpo <fabriciopolicarpo0@gmail.com>
2019-09-05 19:46:59 -0400

    test

--- routes/APIS/chatAPI.js
+++ routes/APIS/chatAPI.js
@@ -5,28 +5,43 @@ const bodyParser = require("body-parser");
 const mongoose = require('mongoose')
 const Room = require('../../models/Room')
 const Lobby = require('../../models/Lobby')
-const url = require('url');
+const url = require('url')
+
 
 router.route("/history").get((req, res, next) => {
-
-  // console.log(req.headers.referer)
   let actualUrl = req.headers.referer;
   var navUrl = url.parse(actualUrl, true);
   let hUrl = navUrl.pathname.split('/');
   let realUrl = hUrl[2];
+  console.log(hUrl)
+
   res.setHeader("Content-Type", "application/json");
   res.statusCode = 200;
-  mongoose.connect('mongodb://localhost/undefined', { useNewUrlParser: true })
-    .then(db => {
-      Lobby.findById(realUrl)
-        .then(theLobby => {
-          Room.find({ _id: { $in: theLobby.messages } })
-            .then(chat => {
-              res.json(chat);
-            }).catch(err => console.log("An error", err))
-        }).catch(otherErr => console.log("Another err ", otherErr));
-    });
+  Lobby.findById(realUrl)
+    .then(theLobby => {
+      Room.find({ _id: { $in: theLobby.messages } })
+        .then(chat => {
+          res.json(chat);
+        }).catch(err => console.log("An error", err))
+    }).catch(otherErr => console.log("Another err ", otherErr));
 });
+
+// router.route("/history").get((req, res, next) => {
+//   let actualUrl = req.headers.referer;
+//   console.log(actualUrl)
+//   var navUrl = actualUrl.parse(actualUrl, true);
+//   // let hUrl = navUrl.pathname.split('/');
+//   // let realUrl = hUrl[2];
+
+//   res.setHeader("Content-Type", "application/json");
+//   res.statusCode = 200;
+//   Lobby.findById()
+
+//   let data = Room.find({ message: "Anonymous" });
+//   Room.find({}).then(chat => {
+//     res.json(chat);
+//   });
+// });
 
 
 
