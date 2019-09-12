ff00538b50f6667279a0182ca60cfd08833bbbba
= <jacex17@gmail.com>
2019-09-12 13:07:46 -0400

    Fix kik function

--- socket/socket.js
+++ socket/socket.js
@@ -81,7 +81,8 @@ function socket(io) {
                 console.log("THE USER WAS");
                 console.log(io.sockets.connected[socket.id]);
                 // io.sockets.connected[data].emit('exitChat', '/allChats'); 
-                io.to(sockets.connected[data]).emit('exitChat', '/allChats');
+               // io.to(sockets.connected[data]).emit('exitChat', '/allChats');
+                io.to.sockets.connected[data].to(realurl).emit('exitChat', '/allChats');
             }
        
                 })
